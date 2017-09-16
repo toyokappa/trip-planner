91ba12bde2c55486f97032550fd910774238f246
toyokappa <kppg42@gmail.com>
2017-09-16 11:03:35 +0900

    Create plans_controller

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,53 @@
+class PlansController < ApplicationController
+  before_action :set_plan, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @plans = Plan.all
+  end
+
+  def show
+  end
+
+  def new
+    @plan = Plan.new
+    @plan.spots.build
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to plan_path(@plan), notice: "登録しました！"
+    else
+      @plan.spots.build
+      render "new"
+    end
+  end
+
+  def edit
+    @plan.spots.build
+  end
+
+  def update
+    if @plan.update(plan_params)
+      redirect_to plan_path(@plan), notice: "更新しました！"
+    else
+      @plan.spots.build
+      render "edit"
+    end
+  end
+
+  def destroy
+    @plan.destroy!
+    redirect_to root_url, notice: "削除しました！"
+  end
+
+  private
+
+    def plan_params
+      params.require(:plan).permit(:name, :description, spots_attributes: [:id, :name, :description, :_destroy])
+    end
+
+    def set_plan
+      @plan = Plan.find(params[:id])
+    end
+end
